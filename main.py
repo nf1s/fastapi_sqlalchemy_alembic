7f86218207519bf8b190d1333195deeb642acc9a
ahmednafies <ahmed.nafies@gmail.com>
2020-02-29 21:28:02 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+import os
+from fastapi_sqlalchemy import DBSessionMiddleware
+from fastapi_sqlalchemy import db
+from models import User as ModelUser
+from schema import User as SchemaUser
+from dotenv import load_dotenv
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+app = FastAPI()
+
+app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
+
+
+@app.post("/user/", response_model=SchemaUser)
+async def create_user(user: SchemaUser):
+    db_user = ModelUser(
+        first_name=user.first_name, last_name=user.last_name, age=user.age
+    )
+    db.session.add(db_user)
+    db.session.commit()
+    return db_user
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
